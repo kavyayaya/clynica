3d8a825830e967999a9663bf60f8d4aba2d26222
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2019-10-05 17:04:09 +0530

    Work with mongo and add basic user operations.

---
+++ Backend/Flask/app/controllers/users.py
@@ -0,0 +1,60 @@
+''' controller and routes for users '''
+import os
+from flask import request, jsonify, make_response
+from app import app, mongo
+import logger
+
+ROOT_PATH = os.environ.get('ROOT_PATH')
+LOG = logger.get_root_logger(
+    __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
+
+
+@app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def user():
+    if request.method == 'GET':
+        query = request.args
+        data = mongo.db.users.find_one(query)
+        if data:
+            return make_response(jsonify(data), 200)
+        else:
+            return make_response(jsonify({'success': False, 'message': 'User not found.'}))
+
+    data = request.get_json()
+    if request.method == 'POST':
+        if data.get('name', None) is not None and data.get('email', None) is not None:
+            # Check if user already exists
+            exist = mongo.db.users.find_one({'email':data.get('email')})
+            # If not, create
+            if not exist:
+                mongo.db.users.insert_one(data)
+                LOG.info("/user POST: User created:" + str(data))
+                return jsonify({'success': True, 'message': 'User created successfully!'}), 200
+            # Otherwise, conflict
+            else:
+                LOG.debug("/user POST: User already exists.")
+                return make_response(jsonify({'success': False, 'message': 'User already exists.'}), 409)
+        else:
+            LOG.debug("/user POST: Bad request parameters.")
+            return jsonify({'success': False, 'message': 'Bad request parameters!'}), 400
+
+    if request.method == 'DELETE':
+        if data.get('email', None) is not None:
+            db_response = mongo.db.users.delete_one({'email': data['email']})
+            if db_response.deleted_count == 1:
+                LOG.info("/user DELETE: User deleted:" + data)
+                response = {'success': True, 'message': 'record deleted'}
+            else:
+                LOG.info("/user DELETE: No record found for deletion: " + data)
+                response = {'success': True, 'message': 'no record found'}
+            return jsonify(response), 200
+        else:
+            LOG.debug("/user DELETE: Bad request parameters.")
+            return jsonify({'success': False, 'message': 'Bad request parameters!'}), 400
+
+    if request.method == 'PATCH':
+        if data.get('query', {}) != {}:
+            mongo.db.users.update_one(
+                data['query'], {'$set': data.get('payload', {})})
+            return jsonify({'success': True, 'message': 'record updated'}), 200
+        else:
+            return jsonify({'success': False, 'message': 'Bad request parameters!'}), 400
