983bfe7975adedec28f0e48c9a5076c820ff3f3c
shreyaspapi <spapinwar@gmail.com>
2019-10-05 17:56:20 +0530

    Init Final Success

--- Frontend/iOS/ClynicaApp/ClynicaApp/AppDelegate.swift
+++ Frontend/iOS/ClynicaApp/ClynicaApp/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  Clynica
+//  ClynicaApp
 //
 //  Created by Winner 77 on 05/10/19.
 //  Copyright © 2019 Beauth. All rights reserved.
--- Frontend/iOS/ClynicaApp/ClynicaApp/ViewController.swift
+++ Frontend/iOS/ClynicaApp/ClynicaApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Clynica
+//  ClynicaApp
 //
 //  Created by Winner 77 on 05/10/19.
 //  Copyright © 2019 Beauth. All rights reserved.
