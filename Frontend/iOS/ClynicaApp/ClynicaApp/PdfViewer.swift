14ec86121d52d774ff3c1a636f7161f13f16b1c1
shreyaspapi <spapinwar@gmail.com>
2019-10-06 12:59:13 +0530

    Final iOS Commit

--- Frontend/iOS/ClynicaApp/ClynicaApp/PdfViewer.swift
+++ Frontend/iOS/ClynicaApp/ClynicaApp/PdfViewer.swift
@@ -14,7 +14,23 @@ class PdfViewer: UIViewController {
     
     @IBOutlet weak var PdfView: PDFView!
     
-    
+    @IBAction func printAction(_ sender: UIButton) {
+        if let guide_url = Bundle.main.url(forResource: "sample", withExtension: "pdf"){
+            if UIPrintInteractionController.canPrint(guide_url) {
+                let printInfo = UIPrintInfo(dictionary: nil)
+                printInfo.jobName = guide_url.lastPathComponent
+                printInfo.outputType = .photo
+
+                let printController = UIPrintInteractionController.shared
+                printController.printInfo = printInfo
+                printController.showsNumberOfCopies = false
+
+                printController.printingItem = guide_url
+
+                printController.present(animated: true, completionHandler: nil)
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
